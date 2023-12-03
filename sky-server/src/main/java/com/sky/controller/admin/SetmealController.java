b870da6a0fa503218670829d99f4cc4ba9a18826
ningxianseng <ng14418hqi02@126.com>
2023-12-03 14:55:33 +0800

    day04
     Integer pageNum = setmealPageQueryDTO.getPage();
            Integer pageSize = setmealPageQueryDTO.getPageSize();
            if(pageNum == null){//若pageNum为空
                pageNum = 1;   //设置默认当前页为1
            }
            if(pageNum <= 0){  //若为负数
                pageNum = 1;   //设置默认值为1
            }
            if(pageSize == null){ //若页面大小为空
                pageSize = 5;    //设置默认每页显示的数据数
            }

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -1,6 +1,8 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.SetmealService;
 import io.swagger.annotations.Api;
@@ -8,10 +10,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.Request;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin/setmeal")
@@ -20,15 +19,41 @@ import org.springframework.web.bind.annotation.RestController;
 public class SetmealController {
 
     @Autowired
-    private SetmealService setmealservice;
+    private SetmealService setmealService;
 
     @PostMapping
     @ApiOperation("新增套餐")
     public Result save(@RequestBody SetmealDTO setmealDTO) {
         log.info("套餐管理新增套餐:{}", setmealDTO);
-        setmealservice.saveWithDish(setmealDTO);
+        setmealService.saveWithDish(setmealDTO);
         return Result.success();
     }
+
+    /**
+     * 分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("分页查询")
+    public Result<PageResult> page(SetmealPageQueryDTO setmealPageQueryDTO) {
+        PageResult pageResult = setmealService.pageQuery(setmealPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
--- sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -1,12 +1,17 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
 import com.sky.mapper.DishMapper;
 import com.sky.mapper.SetmealDishMapper;
 import com.sky.mapper.SetmealMapper;
+import com.sky.result.PageResult;
 import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -49,5 +54,27 @@ public class SetmealServiceImpl implements SetmealService {
         setmealDishMapper.insertBatch(setmealDishes);
     }
 
+
+    /**
+     * 分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    public PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO) {
+        Integer pageNum = setmealPageQueryDTO.getPage();
+        Integer pageSize = setmealPageQueryDTO.getPageSize();
+        if(pageNum == null){//若pageNum为空
+            pageNum = 1;   //设置默认当前页为1
+        }
+        if(pageNum <= 0){  //若为负数
+            pageNum = 1;   //设置默认值为1
+        }
+        if(pageSize == null){ //若页面大小为空
+            pageSize = 5;    //设置默认每页显示的数据数
+        }
+        PageHelper.startPage(pageNum, pageSize);
+        Page<SetmealVO> page = setmealMapper.pageQuery(setmealPageQueryDTO);
+        return new PageResult(page.getTotal(), page.getResult());
+    }
 
 }
